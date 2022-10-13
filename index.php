dc3507eb7de9d6abdccebb9080419868ee733352
kedwynn <kedwynn.nguyen@gmail.com>
2022-10-13 17:35:29 +0200

    validation fiche de frais

--- index.php
+++ index.php
@@ -1,30 +1,27 @@
 <?php
+date_default_timezone_set('Europe/Paris');
 require_once("include/fct.inc.php");
-require_once("include/class.pdogsb.inc.php");
-include("vues/v_entete.php");
+require_once ("include/class.pdogsb.inc.php");
+include("vues/v_entete.php") ;
 session_start();
 $pdo = PdoGsb::getPdoGsb();
 $estConnecte = estConnecte();
-if (!isset($_REQUEST['uc']) || !$estConnecte) {
-	$_REQUEST['uc'] = 'connexion';
-}
+if(!isset($_REQUEST['uc']) || !$estConnecte){
+     $_REQUEST['uc'] = 'connexion';
+}	 
 $uc = $_REQUEST['uc'];
-switch ($uc) {
-	case 'connexion': {
-			include("controleurs/c_connexion.php");
-			break;
-		}
-	case 'gererFrais': {
-			include("controleurs/c_gererFrais.php");
-			break;
-		}
-	case 'etatFrais': {
-			include("controleurs/c_etatFrais.php");
-			break;
-		}
-	case 'validerFrais': {
-			include("controleurs/c_validerFrais.php");
-			break;
-		}
+switch($uc){
+	case 'connexion':{
+		include("controleurs/c_connexion.php");break;
+	}
+	case 'gererFrais' :{
+		include("controleurs/c_gererFrais.php");break;
+	}
+	case 'etatFrais' :{
+		include("controleurs/c_etatFrais.php");break; 
+	}
+	case 'validerFrais' :{
+		include("controleurs/c_validerFrais.php");break;
+	}
 }
-include("vues/v_pied.php");
+include("vues/v_pied.php") ;
--- vues/v_validerFrais.php
+++ vues/v_validerFrais.php
@@ -1,44 +1,60 @@
 <div id="contenu">
-    <h2>Validation des fiches de frais</h2>
-    <fieldset>
-        <legend>
-            <h3>Visiteur et Mois à sélectionner :</h3>
-        </legend>
-        <label>Visiteur : </label>
-        <select id="lstVisiteurs" name="lstVisiteurs">
-            <?php
-            foreach ($lesVisiteurs as $unVisiteur) {
-                $visiteurNom = $unVisiteur['nom'];
-                $visiteurPrenom = $unVisiteur['prenom'];
-            ?>
-                <option selected value=><?php echo $visiteurNom . " " . $visiteurPrenom ?></option>
-            <?php
-            }
-            ?>
-        </select>
+    <h3>Validation des fiches de frais : </h3>
 
+    <form action="index.php?uc=validerFrais&action=afficherFrais" method="post">
+        <div class="corpsForm">
 
+            <fieldset>
+                <legend>Visiteurs et mois à sélectionner</legend>
 
-        <br><br><label for="lstMois" accesskey="n">Mois : </label>
-        <select id="lstMois" name="lstMois">
-            <?php
-            foreach ($lesMois as $unMois) {
-                $mois = $unMois['mois'];
-                $numAnnee =  $unMois['numAnnee'];
-                $numMois =  $unMois['numMois'];
-                if ($mois == $moisASelectionner) {
-            ?>
-                    <option selected value="<?php echo $mois ?>"><?php echo  $numMois . "/" . $numAnnee ?> </option>
-                <?php
-                } else { ?>
-                    <option value="<?php echo $mois ?>"><?php echo  $numMois . "/" . $numAnnee ?> </option>
-            <?php
-                }
-            }
+                <p>
+                    <label for="lstVisiteurs" accesskey="n">Visiteur : </label>
+                    <select id="lstVisiteurs" name="lstVisiteurs">
 
+                        <?php
+                        foreach ($lesVisiteurs as $unVisiteur) {
+                            $visiteurNom = $unVisiteur['nom'];
+                            $visiteurPrenom =  $unVisiteur['prenom'];
+                            $visiteurId = $unVisiteur['id'];
+                            if ($mois == $moisASelectionner) {
+                        ?>
+                                <option selected value="<?= $visiteurId ?>"><?php echo $visiteurPrenom . " " . $visiteurNom ?> </option>
+                            <?php
+                            } else { ?>
+                                <option value="<?= $visiteurId ?>"><?php echo $visiteurPrenom . " " . $visiteurNom ?> </option>
+                        <?php
+                            }
+                        }
+                        ?>
+                    </select>
+                </p>
 
+                <p>
 
-            ?>
-        </select>
-    </fieldset>
-</div>
+                    <label for="lstMois" accesskey="n">Mois : </label>
+                    <select id="lstMois" name="lstMois">
+                        <?php
+                        foreach ($lesMois as $unMois) {
+                            $mois = $unMois['mois'];
+                            $numAnnee =  $unMois['numAnnee'];
+                            $numMois =  $unMois['numMois'];
+                            if ($mois == $moisASelectionner) {
+                        ?>
+                                <option selected value="<?php echo $mois ?>"><?php echo  $numMois . "/" . $numAnnee ?> </option>
+                            <?php
+                            } else { ?>
+                                <option value="<?php echo $mois ?>"><?php echo  $numMois . "/" . $numAnnee ?> </option>
+                        <?php
+                            }
+                        }
+
+                        ?>
+
+                    </select>
+                </p>
+                <div class="piedForm">
+                    <p>
+                        <input id="ok" type="submit" value="Valider" size="20" />
+                    </p>
+                </div>
+        </div>
